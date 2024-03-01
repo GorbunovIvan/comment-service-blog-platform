9929db3d9a7ad5e9653cc253e9ae9924c8ba33df
GorbunovIvan <gorbunovvivan@gmail.com>
2024-03-01 14:24:03 +0200

    dockerization - some fixes and improvements. + logging was added to CommentService

--- src/main/java/org/example/commentservice/service/CommentService.java
+++ src/main/java/org/example/commentservice/service/CommentService.java
@@ -1,6 +1,7 @@
 package org.example.commentservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.example.commentservice.model.Comment;
 import org.example.commentservice.repository.CommentRepository;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class CommentService {
 
     private final CommentRepository commentRepository;
@@ -27,10 +29,13 @@ public class CommentService {
     }
 
     public Comment create(Comment comment) {
-        return commentRepository.save(comment);
+        var commentCreated = commentRepository.save(comment);
+        log.info("Comment created: {}", commentCreated);
+        return commentCreated;
     }
 
     public void deleteById(String id) {
+        log.info("Attempt to delete a comment by id '{}'", id);
         commentRepository.deleteById(id);
     }
 }
